f5efc2a7374eca6fb3935699e11b3c65110dffaf
Damien Huet <huet@math.ubc.ca>
2023-02-01 11:35:44 -0800

    Corrected a segfault

--- Octree/lagrangian_caps/elasticity-ft.h
+++ Octree/lagrangian_caps/elasticity-ft.h
@@ -299,11 +299,8 @@ according to the desired pre-stressed conditions. See the
 an example of a isotropically pre-stressed membrane.*/
 #if dimension > 2
 event init (i = 0) {
-  for(int j=0; j<NCAPS; j++) {
-    fprintf(stderr, "j=%d\n", j);
-    fprintf(stderr, "&MB(j)=%d\n", &MB(j));
-    if (mbs.mb[i].isactive) store_initial_configuration(&MB(j));
-  }
+  for(int j=0; j<NCAPS; j++)
+    if (mbs.mb[j].isactive) store_initial_configuration(&(MB(j)));
 }
 #endif
 
