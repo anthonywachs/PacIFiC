879a278738ca2a884909246fd67a350492880f9b
Damien Huet <huet@math.ubc.ca>
2023-07-18 10:53:00 -0700

    Debug CI attempt 6

--- tests/build_grains.sh
+++ tests/build_grains.sh
@@ -2,8 +2,8 @@ source ../Env/PacIFiC-CI-RUNNER-OpenMPI-2.1.1-GNU-8.2.1.env.sh
 
 # Compilation of Grains3D
 cd $GRAINS_HOME
-tar zxvf xerces-2-8-0.tar.gz
-./makeARCH create ; make update ; make dtd
+tar zxf xerces-2-8-0.tar.gz
+make install
 
 # Check if the compilation was successful
 test -f ${GRAINS_HOME}/Main/bin${GRAINS_FULL_EXT}/grains ; echo $? >> ${PACIFIC_HOME}/tests/build_success.txt
