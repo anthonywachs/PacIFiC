ce4c3a2048a673866729b979cad2eb9af9b16977
Damien Huet <huet@math.ubc.ca>
2023-08-04 10:00:26 +0200

    fix nlp to nln in mpi helper function

--- Octree/lagrangian_caps/capsule-ft-mpi.h
+++ Octree/lagrangian_caps/capsule-ft-mpi.h
@@ -9,19 +9,19 @@ their flow field to the velocities of each Lagrangian nodes.
 
 void reduce_lagVel(lagMesh* mesh) {
   int li = dimension; // length of one item
-  double* send_data = (double*)malloc(mesh->nlp*li*sizeof(double));
-  double* recv_data = (double*)malloc(mpi_npe*mesh->nlp*li*sizeof(double));
-  for(int i=0; i<mesh->nlp; i++) {
+  double* send_data = (double*)malloc(mesh->nln*li*sizeof(double));
+  double* recv_data = (double*)malloc(mpi_npe*mesh->nln*li*sizeof(double));
+  for(int i=0; i<mesh->nln; i++) {
     send_data[li*i] = mesh->nodes[i].lagVel.x;
     send_data[li*i+1] = mesh->nodes[i].lagVel.y;
     #if dimension > 2
     send_data[li*i+2] = mesh->nodes[i].lagVel.z;
     #endif
   }
-  MPI_Allgather(send_data, mesh->nlp*li, MPI_DOUBLE, recv_data,
-    mesh->nlp*li, MPI_DOUBLE, MPI_COMM_WORLD);
+  MPI_Allgather(send_data, mesh->nln*li, MPI_DOUBLE, recv_data,
+    mesh->nln*li, MPI_DOUBLE, MPI_COMM_WORLD);
 
-  for(int i=0; i<mesh->nlp; i++) {
+  for(int i=0; i<mesh->nln; i++) {
     mesh->nodes[i].lagVel.x = 0.;
     mesh->nodes[i].lagVel.y = 0.;
     #if dimension > 2
@@ -30,11 +30,11 @@ void reduce_lagVel(lagMesh* mesh) {
   }
 
   for(int k=0; k<mpi_npe; k++) {
-    for(int i=0; i<mesh->nlp; i++) {
-      mesh->nodes[i].lagVel.x += recv_data[k*li*mesh->nlp+li*i];
-      mesh->nodes[i].lagVel.y += recv_data[k*li*mesh->nlp+li*i+1];
+    for(int i=0; i<mesh->nln; i++) {
+      mesh->nodes[i].lagVel.x += recv_data[k*li*mesh->nln+li*i];
+      mesh->nodes[i].lagVel.y += recv_data[k*li*mesh->nln+li*i+1];
       #if dimension > 2
-      mesh->nodes[i].lagVel.z += recv_data[k*li*mesh->nlp+li*i+2];
+      mesh->nodes[i].lagVel.z += recv_data[k*li*mesh->nln+li*i+2];
       #endif
     }
   }
